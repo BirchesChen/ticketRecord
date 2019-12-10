5c8480fe235f9179e51042442da3b4ac669c5d54
ch <chenhua_receive@163.com>
2019-12-10 18:01:43 +0800

    add sql

--- record.js
+++ record.js
@@ -2,7 +2,8 @@ var request = require('request');
 var cheerio=require("cheerio")
 var mysql=require('mysql')
 var linkArr=[]
-var db=mysql.createPool({host:'localhost',user:'root',password:'123456', port: '3306',database:'recordlist'});
+var dateArr=[]
+var db=mysql.createPool({host:'localhost',user:'root',password:'123456',database:'recordlist',multipleStatements: true});
 
 var options = {
     url: 'http://www.cwl.gov.cn/cwl_admin/kjxx/findDrawNotice?name=ssq&issueCount=30',
@@ -12,7 +13,7 @@ var options = {
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
-        'Cookie': 'UniqueID=jR6Ds4fOmiccd5xe1575359580234; Sites=_21; _ga=GA1.3.1327797372.1575257036; _gid=GA1.3.1060442932.1575359690; _gat_gtag_UA_113065506_1=1; _Jo0OQK=29441419C557E174EF488B01AB22297F4377E29420309CB67C9A4A5B0AE4EAC21C3A3ECE7839EEB4824D3182CF5B273907E4F2ACBF3CF9459464488E4D63F11C867F1B3C19C5B2FC5F8E6E66EDA7420CD4BE6E66EDA7420CD4B2872A3FC3A4A6DA66011E4DFAAE0DD7EGJ1Z1Rg==; 21_vq=46',
+        'Cookie': 'UniqueID=3UMFqaJcjAxxa4u41575957870993; Sites=_21; _ga=GA1.3.1327797372.1575257036; _gid=GA1.3.1670746829.1575879207; _Jo0OQK=ED6F11ED243EF3442EE6E2177A8B416AA4789F4DDD5F1D4B0A2BE6A332114AF77290B04778C5C3FE930480D9FF433B0281EF7D457645E32E67D6F051D01BCDF0770F1B3C19C5B2FC5F8E6E66EDA7420CD4BE6E66EDA7420CD4B502BECCDA3A41CB1CA255D2BC3746F78GJ1Z1fQ==; 21_vq=51',
         'Host': 'www.cwl.gov.cn',
         'Pragma': 'no-cache',
         'Referer': 'http://www.cwl.gov.cn/kjxx/ssq/kjgg/',
@@ -25,14 +26,16 @@ function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
         var result = info.result;
+        console.log(result)
         for(const i in result){
             linkArr.push(result[i].detailsLink);
+            dateArr.push(result[i].code);
         }
-        console.log(linkArr);
         for(const j in linkArr){
             const urls = 'http://www.cwl.gov.cn/'
             const linkDetail = linkArr[j]
             const recordUrl = urls+linkDetail
+            console.log(recordUrl)
             request(recordUrl,function(err,res){
                 if(err)
                 {
@@ -42,6 +45,7 @@ function callback(error, response, body) {
                 {
                     var $ = cheerio.load(res.body, {decodeEntities: false});
                     var arrData = []
+                    //const ele= $('.zjqk').children('table').toString()
                     $('.zjqk').children('table').children('tbody').children('tr').each(function(){
                         var currentRow=$(this);
                         var col1_value=currentRow.find("td").eq(0).text();
@@ -52,14 +56,48 @@ function callback(error, response, body) {
                         obj.中奖注数 =col2_value;
                         obj.金额 = col3_value;
                         arrData.push(obj);
-                        db.query(`INSERT INTO record (奖等, 中奖注数, 金额) VALUE('${col1_value}', '${col2_value}','${col3_value}')`,function(err,data){
-                            if(err)
-                            {
-                                console.log("数据库连接错误");
-                            }
-
-                        })
                     });
+                    console.log(arrData)
+                    console.log('0.0',dateArr)
+                    for(const k in arrData){
+                        const value1=arrData[k].奖等
+                        const value2=arrData[k].中奖注数
+                        const value3=arrData[k].金额
+                        var addSql = "CREATE TABLE "+"table"+[k]+" (level varchar(255),num varchar(255),amount varchar(255))";
+                        db.query(addSql,function(err,data){
+                            if (err) {
+                                console.log("数据库连接错误");
+                                console.log(err)
+                            } else {
+                                console.log("创建表成功")
+                                var addSqls = "insert into record1(level,num,amount) values (?,?,?)";
+                                var addParma = [value1, value2,value3];
+                                db.query(addSqls,addParma,function(err,data){
+                                    if(err){
+                                        console.log("数据库连接错误");
+                                        console.log(err)
+                                    }
+                                })
+                            }
+                        })
+                    }
+                        //ALTER TABLE `record1`;ADD COLUMN `level`  varchar(255) NULL FIRST ;ADD COLUMN `num`  int NULL AFTER `level`;ADD COLUMN `amount`  varchar(255) NULL AFTER `num`;
+                       /* var addSql = "insert into record1(level,num,amount) values (?,?,?)";
+                        var addParmas = [value1, value2,value3];
+                        db.query(addSql,addParmas,function(err,data){
+                            if(err){
+                                console.log("数据库连接错误");
+                                console.log(err)
+                            }
+                        })*/
+                        /*var addSql = "insert into record(level,num,amount) values (?,?,?)";
+                        var addParmas = [value1, value2,value3];
+                        db.query(addSql,addParmas,function(err,data){
+                            if(err){
+                                console.log("数据库连接错误");
+                                console.log(err)
+                            }
+                        })*/
                 }
             });
         }
--- recordList.js
+++ recordList.js
@@ -1,17 +1,89 @@
 const request=require("request")
 const cheerio=require("cheerio")
+var mysql=require('mysql')
 var item=0;
-    request('http://www.cwl.gov.cn/kjxx/ssq/kjgg/',function(err,res) {
-        console.log('in')
-        if (err) {
+var db=mysql.createPool({host:'localhost',user:'root',password:'123456',database:'recordlist',multipleStatements: true});
+
+    /*request(urls,function(err,res){
+        if(err)
+        {
             console.log('请求出错');
         }
-        else {
-            setTimeout(function () {
-                var $ = cheerio.load(res.body, {decodeEntities: false});
-                var element = $('.bgzt table').toString();
-                console.log(element)
-            }, 10000)
+        else
+        {
+            var $ = cheerio.load(res.body, {decodeEntities: false});
+            var arrData = []
+            const ele= $('.zjqk').children('table').toString()
+            console.log(ele)
+            $('.zjqk').children('table').children('tbody').children('tr').each(function(){
+                var currentRow=$(this);
+                var col1_value=currentRow.find("td").eq(0).text();
+                var col2_value=currentRow.find("td").eq(1).text();
+                var col3_value=currentRow.find("td").eq(2).text();
+                var obj={};
+                obj.奖等 = col1_value;
+                obj.中奖注数 =col2_value;
+                obj.金额 = col3_value;
+                arrData.push(obj);
+
+                /!*db.query("INSERT INTO `record` (`level`, `num`, `amount`) VALUES ('${col1_value}', '${col2_value}','${col3_value}');",function(err,data){
+                 if(err)
+                 {
+                 console.log("数据库连接错误");
+                 console.log(err)
+                 }
+
+                 })*!/
+            });
+            console.log(arrData)
+            //console.log('0.0',dateArr)
+            /!*for(const k in arrData){
+                const value1=arrData[k].奖等
+                const value2=arrData[k].中奖注数
+                const value3=arrData[k].金额
+                db.query("CREATE TABLE dateArr[k](level varchar(255),num varchar(255),amount varchar(255))", function(err,result){
+                    if(err){console.log(err)}else{
+                        console.log("创建表成功")
+                    }
+                })*!/
+                //ALTER TABLE `record1`;ADD COLUMN `level`  varchar(255) NULL FIRST ;ADD COLUMN `num`  int NULL AFTER `level`;ADD COLUMN `amount`  varchar(255) NULL AFTER `num`;
+                /!* var addSql = "insert into record1(level,num,amount) values (?,?,?)";
+                 var addParmas = [value1, value2,value3];
+                 db.query(addSql,addParmas,function(err,data){
+                 if(err){
+                 console.log("数据库连接错误");
+                 console.log(err)
+                 }
+                 })*!/
+                /!*var addSql = "insert into record(level,num,amount) values (?,?,?)";
+                 var addParmas = [value1, value2,value3];
+                 db.query(addSql,addParmas,function(err,data){
+                 if(err){
+                 console.log("数据库连接错误");
+                 console.log(err)
+                 }
+                 })*!/
+          /!*  }*!/
+        }
+    });*/
+var arrData=[ 'table1', 'table2', 'table3', 'table4', 'table5' ]
+for(const k in arrData) {
+    var addSql = "CREATE TABLE "+"list"+[k]+" (level varchar(255),num varchar(255),amount varchar(255))";
+    db.query(addSql,function(err,data){
+        if (err) {
+            console.log("数据库连接错误");
+            console.log(err)
+        } else {
+            console.log("创建表成功")
         }
     })
+    /*db.query("CREATE TABLE arrData[k](level varchar(255),num varchar(255),amount varchar(255))", function (err, result) {
+        if (err) {
+            console.log("数据库连接错误");
+            console.log(err)
+        } else {
+            console.log("创建表成功")
+        }
+    })*/
+}
 
